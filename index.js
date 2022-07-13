d01ba9063ec0f3c9632f5b382a7c07d88d27c479
fatihguzel <20060393@stu.omu.edu.tr>
2022-07-13 04:46:19 +0300

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { appendFile } = require("fs")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+const path = require("path")
+app.use('/css',express.static(path.join(__dirname, 'public/styles.css')));
+app.use('/css',express.static(path.join(__dirname, '/app.js')));
+
+
+app.get("/",async(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.sendFile(path.resolve(__dirname+ '/index.html'))
+}).listen(5040)
+
+
+console.log("Listening on PORT 5040");
