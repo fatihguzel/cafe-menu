11a25e04c2056b23d708c0ac1f55783924f97471
fatihguzel <20060393@stu.omu.edu.tr>
2022-07-13 21:34:51 +0300

    update

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const { appendFile } = require("fs")
+function open(){
+    const express = require("express")
+    const bodyParser = require("body-parser")
+    const { appendFile } = require("fs")
+    
+    const app = express()
+    
+    app.use(bodyParser.json())
+    app.use(express.static('public'))
+    app.use(bodyParser.urlencoded({
+        extended:true
+    }))
+    const path = require("path")
+    app.use('/css',express.static(path.join(__dirname, 'public/styles.css')));
+    app.use('/css',express.static(path.join(__dirname, '/app.js')));
+    app.use('/css',express.static(path.join(__dirname, 'public/images/indir.jpg')));
+    
+    
+    app.get("/",async(req,res)=>{
+        res.set({
+            "Allow-access-Allow-Origin": '*'
+        })
+        return res.sendFile(path.resolve(__dirname+ '/public/index.html'))
+    }).listen(5040)
+    
+    
+    console.log("Listening on PORT 5040");
+}
 
-const app = express()
-
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
-const path = require("path")
-app.use('/css',express.static(path.join(__dirname, 'public/styles.css')));
-app.use('/css',express.static(path.join(__dirname, '/app.js')));
-app.use('/css',express.static(path.join(__dirname, 'public/images/indir.jpg')));
-
-
-app.get("/",async(req,res)=>{
-    res.set({
-        "Allow-access-Allow-Origin": '*'
-    })
-    return res.sendFile(path.resolve(__dirname+ '/public/index.html'))
-}).listen(5040)
-
-
-console.log("Listening on PORT 5040");
+open()
